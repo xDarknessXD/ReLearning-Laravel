f3fbaa576e23499309a7b574cb0ddf6d93e8378e
Darkq <113276529+xDarknessXD@users.noreply.github.com>
2024-09-03 21:11:28 +0800

    Day 9
    Migration of the table

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,39 +6,9 @@ use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
+    protected $table = 'job_listings';
 
-            [
-                'id' => '1',
-                'title' => 'Senior Software Engineer',
-                'salary' => '30,000'
-            ],
-            [
-                'id' => '2',
-                'title' => 'Software Engineer',
-                'salary' => '20,000'
-            ],
-            [
-                'id' => '3',
-                'title' => 'Frontend Developer',
-                'salary' => '10,000'
-            ],
-
-
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (! $job) {
-            abort(404);
-        };
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary',];
 };
