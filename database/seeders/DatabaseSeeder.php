324540e9971efd952d76f1c1156bee61b9aaa246
Darkq <113276529+xDarknessXD@users.noreply.github.com>
2024-09-19 03:05:16 +0800

    Day 15
    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'John',
+            'last_name' => 'Doeee',
+            'email' => 'test@example',
         ]);
+
+        $this->call(JobSeeder::class);
+
     }
 }
